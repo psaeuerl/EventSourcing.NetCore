83f4ef564f5d6ecd57dc23e3fa221f83175c491f
psaeutt <psaeu@techtalk.at>
2024-05-18 14:37:27 +0200

    myapproach

--- Workshops/IntroductionToEventSourcing/05-GettingStateFromEvents.Marten/Immutable/GettingStateFromEventsTests.cs
+++ Workshops/IntroductionToEventSourcing/05-GettingStateFromEvents.Marten/Immutable/GettingStateFromEventsTests.cs
@@ -53,7 +53,66 @@ public record ShoppingCart(
     PricedProductItem[] ProductItems,
     DateTime? ConfirmedAt = null,
     DateTime? CanceledAt = null
-);
+
+
+)
+{
+    private ShoppingCart() : this(Guid.Empty, Guid.Empty, ShoppingCartStatus.Pending, Array.Empty<PricedProductItem>()) { } // Let's make Marten happy
+
+    public ShoppingCart Apply(ShoppingCartEvent @event)
+    {
+        switch (@event)
+        {
+            case ProductItemAddedToShoppingCart productItemAddedToShoppingCart:
+                return this with
+                {
+                    ProductItems = new[] { productItemAddedToShoppingCart.ProductItem }.Concat(ProductItems)
+                        .ToArray()
+                };
+            case ProductItemRemovedFromShoppingCart productItemRemovedFromShoppingCart:
+                var productItemProductId = productItemRemovedFromShoppingCart.ProductItem.ProductId;
+                var amountStored = ProductItems.Single(x => x.ProductId == productItemProductId)
+                    .Quantity;
+                var newQuantity = amountStored - productItemRemovedFromShoppingCart.ProductItem.Quantity;
+                var productItemsWithoutOld = ProductItems
+                    .Where(x => x.ProductId != productItemProductId).ToArray();
+                if (newQuantity < 0)
+                {
+                    return this with { ProductItems = productItemsWithoutOld };
+                }
+
+                var productItemsUpdated = new[]
+                {
+                    new PricedProductItem(productItemProductId, newQuantity,
+                        productItemRemovedFromShoppingCart.ProductItem.UnitPrice)
+                }.Concat(productItemsWithoutOld).ToArray();
+
+                return this with { ProductItems = productItemsUpdated };
+            case ShoppingCartCanceled shoppingCartCanceled:
+                return this with
+                {
+                    Status = ShoppingCartStatus.Canceled,
+                    CanceledAt = shoppingCartCanceled.CanceledAt,
+                    ConfirmedAt = null
+                };
+            case ShoppingCartConfirmed shoppingCartConfirmed:
+                return this with
+                {
+                    Status = ShoppingCartStatus.Confirmed,
+                    ConfirmedAt = shoppingCartConfirmed.ConfirmedAt,
+                    CanceledAt = null
+                };
+            case ShoppingCartOpened shoppingCartOpened:
+                return new ShoppingCart(
+                    shoppingCartOpened.ShoppingCartId
+                    , shoppingCartOpened.ClientId,
+                    ShoppingCartStatus.Pending,
+                    Array.Empty<PricedProductItem>());
+            default:
+                throw new ArgumentOutOfRangeException(nameof(@event));
+        }
+    }
+}
 
 public enum ShoppingCartStatus
 {
@@ -71,13 +130,18 @@ public class GettingStateFromEventsTests: MartenTest
     /// <param name="shoppingCartId"></param>
     /// <param name="cancellationToken"></param>
     /// <returns></returns>
-    private static Task<ShoppingCart> GetShoppingCart(
+    private static  Task<ShoppingCart> GetShoppingCart(
         IDocumentSession documentSession,
         Guid shoppingCartId,
         CancellationToken cancellationToken)
     {
         // 1. Add logic here
-        throw new NotImplementedException();
+        //My Approach
+        //var events = await documentSession.Events.FetchStreamAsync(shoppingCartId, token: cancellationToken);
+        //return ShoppingCart.From(events.Select(x => (ShoppingCartEvent)x.Data).ToArray());
+
+        var shoppingCart = documentSession.Events.AggregateStreamAsync<ShoppingCart>(shoppingCartId, token: cancellationToken);
+        return (shoppingCart ?? throw new InvalidOperationException("Shopping Cart was not found!"))!;
     }
 
     [Fact]
